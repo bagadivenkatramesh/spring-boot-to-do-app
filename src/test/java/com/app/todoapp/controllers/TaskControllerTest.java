016b58e176a69c0a5d44be75dc866849735a5176
bagadivenkatramesh <bvrameshhh@gmail.com>
2026-09-07 23:07:40 +0530

    added initial test files

---
+++ src/test/java/com/app/todoapp/controllers/TaskControllerTest.java
@@ -0,0 +1,28 @@
+package com.app.todoapp.controllers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TaskControllerTest {
+
+    @Test
+    void getTasks() {
+    }
+
+    @Test
+    void createTask() {
+    }
+
+    @Test
+    void deleteTask() {
+    }
+
+    @Test
+    void updateTask() {
+    }
+
+    @Test
+    void toggleTask() {
+    }
+}
---
+++ src/test/java/com/app/todoapp/services/TaskServiceTest.java
@@ -0,0 +1,28 @@
+package com.app.todoapp.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TaskServiceTest {
+
+    @Test
+    void getAllTasks() {
+    }
+
+    @Test
+    void createTask() {
+    }
+
+    @Test
+    void deleteTask() {
+    }
+
+    @Test
+    void updateTask() {
+    }
+
+    @Test
+    void toggleTask() {
+    }
+}
